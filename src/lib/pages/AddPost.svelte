dfad3806399bc9eef9177ab5c81ffc258a6cbdf2
khubaib01 <ignitedmind350@gmail.com>
2022-06-30 22:12:38 +0600

    modified addpost

--- src/lib/pages/AddPost.svelte
+++ src/lib/pages/AddPost.svelte
@@ -1,29 +1,39 @@
-<section
-  class="container max-w-screen-xl flex flex-col items-start gap-y-7 mt-8"
->
-  <h1 class="fade-in-top text-2xl md:text-5xl">Add Post</h1>
-  <div class="w-full">
-    <label for="title" class="text-lg">Post title</label><input
-      type="text"
-      name="title"
-      placeholder="Post title"
-      class="input text-lg mt-2"
-    />
-  </div>
-  <div class="w-full">
-    <label for="tags" class="text-lg">Search hash tags</label><input
-      type="text"
-      name="tags"
-      placeholder="Search tags"
-      class="input text-lg mt-2"
-    />
-  </div>
-  <div class="w-full">
-    <label for="post" class="text-lg">Post body</label><textarea
-      type="text"
-      name="post"
-      placeholder="Post body"
-      class="input text-lg mt-2 min-h-[300px]"
-    />
+<script lang="ts">
+  let title: string;
+  let tags: string;
+  let postbody: string;
+</script>
+
+<section class="container max-w-screen-xl mt-8">
+  <div class="flex flex-col items-start gap-y-7 mx-4 md:mx-0">
+    <h1 class="fade-in-top text-2xl md:text-5xl">Add Post</h1>
+    <div class="w-full">
+      <label for="title" class="text-lg">Post title</label><input
+        type="text"
+        name="title"
+        bind:value={title}
+        placeholder="Post title"
+        class="input text-lg mt-2"
+      />
+    </div>
+    <div class="w-full">
+      <label for="tags" class="text-lg">Search hash tags</label><input
+        type="text"
+        name="tags"
+        bind:value={tags}
+        placeholder="Search tags"
+        class="input text-lg mt-2"
+      />
+    </div>
+    <div class="w-full">
+      <label for="post" class="text-lg">Post body</label><textarea
+        type="text"
+        name="post"
+        bind:value={postbody}
+        placeholder="Post body"
+        class="input text-lg mt-2 min-h-[300px]"
+      />
+    </div>
+    <button class="btn-rounded w-full">Post</button>
   </div>
 </section>
